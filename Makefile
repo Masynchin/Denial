ef7a21793148b9c3d8c47bc5ee0f8f86f784be92
Max Smirnov <masynchin@gmail.com>
2023-02-16 16:26:06 +0300

    Welcome Caesar

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-.PHONY: number bars point
+.PHONY: number caesar bars point
 
 number:
 	g++ ./solutions/number.cpp -o ./main.o -std=c++11 && ./main.o
+
+caesar:
+	g++ ./solutions/caesar.cpp -o ./main.o -std=c++11 && ./main.o
 
 bars:
 	g++ ./solutions/bars.cpp -o ./main.o -std=c++11 && ./main.o
---
+++ solutions/caesar.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    wstring source;
+    wcout << L"Введите фразу для шифрования/расшифрования: ";
+    getline(wcin, source);
+
+    int shift;
+    wcout << L"Введите сдвиг (вправо - положительный, влево - отрицательный): ";
+    wcin >> shift;
+    shift = shift % 32 + 32;
+
+    wstring opType;
+    wcout << L"Хотите зашифровать (0), или расшифровать (1) фразу: ";
+    wcin >> opType;
+
+    if (opType == L"0") {
+        for (wchar_t& letter : source) {
+            if (letter >= L'А' && letter <= L'Я') {
+                letter = L'А' + ((letter - L'А' + shift) % 32);
+            } else if (letter >= L'а' && letter <= L'я') {
+                letter = L'а' + ((letter - L'а' + shift) % 32);
+            }
+        }
+        wcout << L"Зашифрованная фраза: " << source << endl;
+    } else if (opType == L"1") {
+        for (wchar_t& letter : source) {
+            if (letter >= L'А' && letter <= L'Я') {
+                letter = L'А' + ((letter - L'А' - shift) % 32);
+            } else if (letter >= L'а' && letter <= L'я') {
+                letter = L'а' + ((letter - L'а' - shift) % 32);
+            }
+        }
+        wcout << L"Расшифрованная фраза: " << source << endl;
+    }
+    else {
+        wcout << L"Неверный ввод операции";
+    }
+
+    return 0;
+}
